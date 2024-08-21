8ee5f3fafcebbc1c09560397181880d8a9ca6fd9
Takeru Kurihara <t.kurihara@hybrid-technologies.co.jp>
2024-08-21 23:53:33 +0900

    Using the Blade template

--- laravel-study/routes/web.php
+++ laravel-study/routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/hello-world', function () {
     return 'hello world';
 });
+
+Route::get('/',fn() => view(view: 'index'));
+Route::get('/curriculum',fn() => view(view: 'curriculum'));
